eb3c5c5b53ea22e6f59277c984f2ac003c953cf1
pedrosouzax <pedroh.soouza@gmail.com>
2021-03-13 18:59:36 -0300

    Getting the params actors, genre and rating from main page

--- movie_now.py
+++ movie_now.py
@@ -1,7 +1,17 @@
 from  flask import Flask
 from flask import render_template
+from flask import request
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_movie():
     return render_template('index.html')
+
+@app.route("/searching.html")
+def searching():
+    actors = request.args.get("actors")
+    rating = request.args.get("rating")
+    genre = request.args.get("genre")
+    print(genre,rating,actors)
+    return render_template("index.html")

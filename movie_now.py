d8da8211d988fee36f2fb72dc6c4faee037aeea9
pedrosouzax <pedroh.soouza@gmail.com>
2021-03-27 17:50:35 -0300

    Getting all information about the selected movie and displaying it. Now, I just need to work on design.

--- movie_now.py
+++ movie_now.py
@@ -11,14 +11,14 @@ def hello_movie():
 
 @app.route("/searching.html")
 def searching():
-    actors = request.args.get("actors")
     rating = request.args.get("rating")
     genre = request.args.get("genre")
-    content = sm.searching(genre=genre.lower(),rating=rating)
-    # movie, rating, img = 
-    movie = sm.get_movie(content)
+    streamer = request.args.get("streamer")
+    # movie_tvshow = request.args.get("movie-tvshow")
+    content = sm.searching(genre=genre.lower(),rating=rating,streamer=streamer)
+    name,folder,year,imdb = sm.get_movie(content)
     # print(actors, rating, genre)
-    return render_template("searching.html",movie=movie)
+    return render_template("searching.html",title=name,folder=folder, year=year, imdb=imdb)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- search_movies.py
+++ search_movies.py
@@ -11,6 +11,7 @@ def searching(genre="all", rating="0", year="1900", movie_or_tvshow="movies", st
         else:
             print(f"https://reelgood.com/{movie_or_tvshow}/genre/{genre}/on-{streamer}?filter-imdb_start={rating}&filter-sort=2")
             response = requests.get(f"https://reelgood.com/{movie_or_tvshow}/genre/{genre}/on-{streamer}?filter-imdb_start={rating}&filter-sort=2")
+            print("aaaa")
             return response.content
     except:
         return "We can't find any movies :( Please, try again!"
@@ -18,7 +19,17 @@ def searching(genre="all", rating="0", year="1900", movie_or_tvshow="movies", st
 def get_movie(content):
     soup =  BeautifulSoup(content,'html.parser')
     movies_list = []
-    movies_list = [soup.find_all("td",class_="css-1u7zfla e126mwsw1")[x].a.string for x in range(len(soup.find_all("td",class_="css-1u7zfla e126mwsw1")))]
-    movie = random.choice(movies_list)
-    return movie
+    movies_list = [soup.find_all("tr",class_="css-o6sgwe")[x] for x in range(len(soup.find_all("tr",class_="css-o6sgwe")))]
+    #print(movies_list,"\n\n")
+    movie_tr = random.choice(movies_list)
+    movie_name = movie_tr.find("td",class_="css-1u7zfla e126mwsw1").a.string
+    movie_folder = movie_tr.find("picture", class_="css-b4kcmh e1181ybh0").img["src"]
+    movie_year = movie_tr.find("td",class_="css-1u11l3y").string
+    movie_imdb = movie_tr.find("b",class_="css-1px39yc").div.span.b.string
+    # movie_avaliable = movie_tr.find("td", class_="css-1vuzpp2").span.div.img["alt"]
+    # print(movie_name,"\n\n")
+    # print(movie_year,"\n\n")
+    # print(movie_imdb,"\n\n")
+    # print(movie_folder,"\n\n")
+    return movie_name,movie_folder, movie_year,movie_imdb
     
